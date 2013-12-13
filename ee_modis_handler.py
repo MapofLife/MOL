afe60292f489310d187bd7137b21c68fc35348d0
benscarlson <ben.s.carlson@gmail.com>
2013-12-13 16:59:55 -0500

    update to parse cdb points and intersect with range map on ee

--- ee_modis_handler.py
+++ ee_modis_handler.py
@@ -21,19 +21,17 @@ class MainPage(webapp2.RequestHandler):
         self.response.out.write(template.render(path, template_args))
 
     def getRandomPoints(self,sciname):
-        cdburl = 'http://mol.cartodb.com/api/v1/sql?q=%s'
+        cdburl = 'https://mol.cartodb.com/api/v1/sql?q=%s'
         sql = "Select " \
             "ST_X(ST_Transform(the_geom_webmercator,4326)) as lon, " \
-            "ST_Y(ST_Transform(the_geom_webmercator,4326)) as lat, " \
-            "1 as val " \
+            "ST_Y(ST_Transform(the_geom_webmercator,4326)) as lat " \
             "FROM get_tile_beta('gbif_aug_2013','%s') " \
             "order by random() limit 1000"
-        url =  cdburl % (sql % (sciname))
-            
-        self.points = urlfetch.fetch(url)
-        logging.info(url)
-        logging.info(self.points)
-        logging.info("HELLO!!!!")
+        qstr = urllib.quote_plus((sql % (sciname)))
+        url = cdburl % (qstr)
+
+        points = urlfetch.fetch(url)
+        return points.content
         
     def get(self):
 
@@ -50,19 +48,20 @@ class MainPage(webapp2.RequestHandler):
         cover = ee.Image('MCD12Q1/MCD12Q1_005_%s_01_01' % (year)).select('Land_Cover_Type_1')
         elev = ee.Image('srtm90_v4')
 
+        #define a global polygon for the reducers
+        region = ee.Feature(ee.Feature.Polygon([[-179.9,-89.9],[-179.9,89.9],[179.9,89.9],[179.9, -89.9], [-179.9, -89.9]]))
+        geometry = region.geometry()
+        
         output = ee.Image(0)
         empty = ee.Image(0).mask(0)
-
 
         species = ee.Image(ee_id)
 
         #parse the CDB response
 
-
         min = int(elevation.split(',')[0])
         max = int(elevation.split(',')[1])
         habitat_list = habitats.split(",")
-
 
         output = output.mask(species)
 
@@ -73,10 +72,27 @@ class MainPage(webapp2.RequestHandler):
 
         if(get_area == 'false'):
             
-            self.getRandomPoints(sciname)
-            # TODO: next:
-            # do something with self.points in ee 
-            # paint points into result
+            pointjsons = self.getRandomPoints(sciname)
+            pjson = json.loads(pointjsons)
+            eePts = []
+            pTmpl = "ee.Feature(ee.Feature.Point(%f,%f))"
+            
+            for row in pjson["rows"]:
+                eePts.append(ee.Feature(ee.Feature.Point(row["lon"],row["lat"]),{'val':1}))
+            
+            eePtFc = ee.FeatureCollection(eePts)
+            
+            #this code reduces the point collection to an image.  Each pixel 
+            #contains a count for the number of pixels that intersect with it
+            #then, the point count image is masked by the range
+            imgPoints = eePtFc.reduceToImage(['val'], ee.call('Reducer.sum')).mask(result);
+            
+            #now, just sum up the points image.  this is the number of points that overlap the range
+            ptsIn = imgPoints.reduceRegion(ee.call('Reducer.sum'), geometry, 10000)
+
+            logging.info("Point In Range: " + str(ptsIn.getInfo()['sum']))
+
+            #TODO: paint points into result
             
                
             mapid = result.getMapId({
@@ -99,11 +115,8 @@ class MainPage(webapp2.RequestHandler):
             
             area = area.mask(species)
             total = area.mask(result.mask())
-          
-            region = ee.Feature(ee.Feature.Polygon([[-179.9,-89.9],[-179.9,89.9],[179.9,89.9],[179.9, -89.9], [-179.9, -89.9]]))
-	    geometry = region.geometry()
 
-            ##compute area on 1km scale
+            ##compute area on 10km scale
             clipped_area = total.reduceRegion(sum_reducer,geometry,10000)
 	    total_area = area.reduceRegion(sum_reducer,geometry,10000)
 
